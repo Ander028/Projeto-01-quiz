e3fc88236c7e868874a17c7aed5d728d71786ed1
Ander028 <Andrezzaoabc@gmail.com>
2023-09-12 10:01:36 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './styles/global.css'
+
+function MeuNome(){
+  return(
+    <h2>André Luis</h2>
+  )
+}
+function App() {
+  return (
+    <>
+    <h1>Hello word</h1>
+    <MeuNome/>
+    </>
+  )
+}
+
+export default App
